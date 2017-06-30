99d64edb51aaee26272a7c3a20feb9d42c5c292c
Guilherme <guilherme.soares@linea.gov.br>
2017-06-30 17:41:14 -0300

    Adicionei jquery e jquery-ui e mudei o header e o footer

--- wp-content/themes/LIneA/header.php
+++ wp-content/themes/LIneA/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//PT">
-<!-- 
+<!--
 ######################################
 #   Este site foi desenvolvido por   #
 # Mairus "Webber" Maichrovicz Cabral #
@@ -14,10 +14,11 @@
 		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/lightbox.css" type="text/css" media="screen" />
 		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/owl.carousel.css" type="text/css" media="screen" />
 		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/owl.theme.default.min.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/jquery-ui.min.css" type="text/css" media="screen" />
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,700,400' rel='stylesheet' type='text/css'>
 		<meta name="viewport" content="width=device-width" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
-		
+
 		<?php
 		  $bg = array('DES_img01_alt.png', 'DES_img02_alt.png', 'DES_img07_alt.png'); // array of filenames
 
@@ -25,10 +26,9 @@
 		  $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
 		?>
 		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/prototype.js"></script>
-		
+
 		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/scriptaculous.js?load=effects,builder"></script>
 		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/lightbox.js"></script>
-		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 		<script>
 			function change(id, newClass) {
 				identity=document.getElementById(id);
