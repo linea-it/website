0d0b5509e1e6b62705f3a2bea327596db6c0baf1
Guilherme <guilherme.soares@linea.gov.br>
2017-07-13 14:26:53 -0300

    Seletor de TNO com nomes alternativos

--- wp-content/themes/LIneA/tno-acesso-dados/tno-filtro.php
+++ wp-content/themes/LIneA/tno-acesso-dados/tno-filtro.php
@@ -14,6 +14,45 @@ function get_tno_nomes($con) {
   return $lista_tno_nomes;
 }
 
+function get_tno_nomes_alternativos($con) {
+  $sql = 'SELECT nome_tno, nome_alternativo FROM tno_info';
+  $prep = $con->prepare($sql);
+  $prep->execute();
+  $result = $prep->fetchAll();
+  $lista_tno_nomes_alternativos = array();
+  foreach ($result as $row) {
+    $lista_tno_nomes_alternativos[$row['nome_tno']] = $row['nome_alternativo'];
+  }
+  return $lista_tno_nomes_alternativos;
+}
+
+function compara_nomes_tno($a, $b) {
+  $pattern = '/(^\d+)_/';
+  preg_match($pattern, $a, $a_matches);
+  preg_match($pattern, $b, $b_matches);
+  $a_match = $a_matches[1];
+  $b_match = $b_matches[1];
+  if ( is_numeric($a_match) && !is_numeric($b_match) ) {
+    return 1;
+  } else if ( !is_numeric($a_match) && is_numeric($b_match) ){
+    return -1;
+  } else {
+    return ( $a < $b )? -1 : 1;
+  }
+}
+
+function merge_nomes_alternativos($lista_tno_nomes, $lista_tno_nomes_alternativos) {
+  $lista = array();
+  foreach ($lista_tno_nomes as $nome_tno) {
+    if ( array_key_exists($nome_tno, $lista_tno_nomes_alternativos) ) {
+      $nome_tno = $lista_tno_nomes_alternativos[$nome_tno];
+    }
+    array_push($lista, $nome_tno);
+  }
+  usort($lista, 'compara_nomes_tno');
+  return $lista;
+}
+
 function get_tno_mapas($con, $nome_tno, $data_inicio, $data_fim, $limite, $pagina) {
   $sql = 'SELECT * FROM tno_objetos WHERE nome_tno = ? and data >= ? and data <= ? and tipo_arquivo = ? ORDER BY data LIMIT ?, ?';
   $prep = $con->prepare($sql);
@@ -66,15 +105,20 @@ function cria_paginacao($limite, $pagina, $total_imagens) {
   return $str_paginacao;
 }
 
-function tno_objetos_select($lista_tno_nomes, $nome_selecionado) {
+function tno_objetos_select($lista_tno_nomes, $lista_tno_nomes_alternativos, $nome_selecionado) {
   $str_select = '';
   $str_select .= '<select class="tno-sel" name="nome_tno">';
   foreach ($lista_tno_nomes as $nome_tno) {
+    $value = $nome_tno;
+    $nome_alternativo_key = array_search($nome_tno, $lista_tno_nomes_alternativos);
+    if ( $nome_alternativo_key !== FALSE ) {
+      $value = $nome_alternativo_key;
+    }
     $selecionado = '';
-    if ($nome_selecionado == $nome_tno) {
+    if ($nome_selecionado == $value) {
       $selecionado = 'selected';
     }
-    $str_select .= '<option value="' . $nome_tno . '" ' . $selecionado . '>' . $nome_tno;
+    $str_select .= '<option value="' . $value . '" ' . $selecionado . '>' . $nome_tno;
     $str_select .= '</option>';
   }
   $str_select .= '</select>';
