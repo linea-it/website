8b372b49f7a2ef748f6d5968a006faf4b2f677a3
Guilherme <guilherme.soares@linea.gov.br>
2017-08-10 15:01:16 -0300

    Pagina de eventos dinamica com alter, editar e deletar

---
+++ wp-content/themes/LIneA/eventos_create.php
@@ -0,0 +1,141 @@
+<?php
+
+require( $_SERVER['DOCUMENT_ROOT'].'/wp-load.php' );
+if (!is_user_logged_in()) {
+	header("Location: /");
+}
+
+?>
+<?php get_header(); ?>
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<?php get_sidebar(); ?>
+<div class="clearboth"></div>
+	<div id="content" class="conteudo create page" role="main">
+			<?php
+				require 'database.php';
+				require 'linea_func.php';
+
+			if ( !empty($_POST)) {
+			    // keep track validation errors
+			    $tituloError = null;
+			    $linkError = null;
+
+			    // keep track post values
+			    $titulo = $_POST['titulo'];
+			    $data_inicial = $_POST['data_inicial'];
+			    $data_final = $_POST['data_final'];
+					$local = $_POST['local'];
+					$envolvimento = $_POST['envolvimento'];
+					$porte = $_POST['porte'];
+			    $link = $_POST['link'];
+
+			    // validate input
+			    $valid = true;
+			    if (empty($titulo)) {
+			        $tituloError = 'Insira o título';
+			        $valid = false;
+			    }
+					if (!empty($link)) {
+						if (!filter_var($link, FILTER_VALIDATE_URL)) {
+						    $linkError = "$link is not a valid URL";
+								$valid = false;
+						}
+					}
+
+
+			    // update data
+			    if ($valid) {
+			        $pdo = Database::connect();
+			        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			        $sql = "INSERT INTO eventos (titulo, data_inicial, data_final, local, envolvimento, porte, link) values (?, ?, ?, ?, ?, ?, ?)";
+			        $q = $pdo->prepare($sql);
+			        $q->execute(array($titulo, $data_inicial, $data_final, $local, $envolvimento, $porte, $link));
+
+			        // Inserindo LOG da operação no banco
+			        $sql_log = "INSERT INTO log (wp_username, datetime, action, page, resumo) VALUES (?, now(), 'INSERT', 'EVENTOS', ?)";
+			        $q_log = $pdo->prepare($sql_log);
+
+			        $current_user = wp_get_current_user();
+			        $wp_username = $current_user->user_login;
+
+			        $q_log->execute(array($wp_username, resumo($titulo)));
+
+			        Database::disconnect();
+			        header("Location: /eventos/");
+			    }
+			}
+			?>
+
+		    <main class="container">
+		    <section>
+		        <h1>Create</h1>
+
+		        <form action="<?php echo get_bloginfo('template_url') . '/eventos_create.php' ?>" method="post">
+
+							<!-- Titulo -->
+							<div class="grupo-input <?php echo !empty($tituloError)?'error':'';?>">
+								<label for="titulo" class="control-label">Titulo:</label>
+								<input id="titulo" name="titulo" type="text"  placeholder="Titulo" value="<?php echo !empty($titulo)?$titulo:'';?>">
+								<?php if (!empty($tituloError)): ?>
+								    <span class="errormsg"><?php echo $tituloError;?></span>
+								<?php endif; ?>
+							</div>
+
+							<!-- Local -->
+							<div class="grupo-input <?php echo !empty($localError)?'error':'';?>">
+								<label for="local" class="control-label">Local:</label>
+								<input id="local" name="local" type="text"  placeholder="ex.: LIneA, Rio de Janeiro, Brasil" value="<?php echo !empty($local)?$local:'';?>">
+								<?php if (!empty($localError)): ?>
+								    <span class="errormsg"><?php echo $localError;?></span>
+								<?php endif; ?>
+							</div>
+
+							<!-- Data Inicial -->
+							<div class="grupo-input">
+		  					<label>Data inicial:</label>
+		  					<input class="datepicker" type="text" name="data_inicial" placeholder="AAAA-MM-DD" value="<?php echo !empty($data_inicial)?$data_inicial:'';?>">
+		  				</div>
+
+							<!-- Data Final -->
+		  				<div class="grupo-input">
+		  					<label>Data final</label>
+		  					<input class="datepicker" type="text" name="data_final" placeholder="AAAA-MM-DD" value="<?php echo !empty($data_final)?$data_final:'';?>">
+		  				</div>
+
+							<!-- Envolvimento -->
+							<div class="grupo-input">
+								<label for="envolvimento" class="control-label">Envolvimento:</label>
+								<select name="envolvimento" id="envolvimento">
+									<option value="Organizacao" <?php echo $envolvimento == 'Organizacao'?'selected':''; ?> >Organizacao</option>
+									<option value="Participacao" <?php echo $envolvimento == 'Participacao'?'selected':''; ?> >Participacao</option>
+								</select>
+							</div>
+
+							<!-- Porte -->
+							<div class="grupo-input">
+								<label for="porte" class="control-label">Envolvimento:</label>
+								<select name="porte" id="porte">
+									<option value="Nacional" <?php echo $porte == 'Nacional'?'selected':''; ?> >Nacional</option>
+									<option value="Internacional" <?php echo $porte == 'Internacional'?'selected':''; ?> >Internacional</option>
+								</select>
+							</div>
+
+							<!-- Link -->
+							<div class="grupo-input">
+								<label for="link" class="control-label">Link:</label>
+								<input id="link" name="link" type="text"  placeholder="Link" value="<?php echo !empty($link)?$link:'';?>">
+								<?php if (!empty($linkError)): ?>
+								    <span class="errormsg"><?php echo $linkError;?></span>
+								<?php endif; ?>
+							</div>
+
+	          	<div class="form-actions">
+	              <button type="submit" class="btn">Create</button>
+	              <a class="btn" href="/eventos/">Back</a>
+	            </div>
+		        </form>
+		    </section>
+		    </main>
+	</div>
+	<div class="clearboth"></div>
+<?php get_footer(); ?>
---
+++ wp-content/themes/LIneA/eventos_functions.php
@@ -0,0 +1,66 @@
+<?php
+
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+
+function get_eventos_envolvimento($con, $envolvimento) {
+  $sql = "SELECT * FROM eventos WHERE envolvimento = :envolvimento ORDER BY data_inicial DESC";
+  $prep = $con->prepare($sql);
+  $prep->bindParam(':envolvimento', $envolvimento, PDO::PARAM_STR);
+  $prep->execute();
+  $result = $prep->fetchAll();
+  return $result;
+}
+
+function get_org_porte($organizacao, $porte) {
+  $array_porte = array();
+  foreach ($organizacao as $row_org) {
+    if ($row_org['porte'] == $porte) {
+      array_push($array_porte, $row_org);
+    }
+  }
+  return $array_porte;
+}
+
+function show_evento($row_evento, $login) {
+  $data_inicial = strtotime($row_evento['data_inicial']);
+  $data_final = strtotime($row_evento['data_final']);
+  $elemento = '';
+  $elemento .= '<div class="evento">';
+  $elemento .= '<a href="' . $row_evento['link'] . '">';
+  $elemento .= '<span class="evento-titulo">' . $row_evento['titulo'] . '</span>';
+  $elemento .= '</a>';
+  $elemento .= '<span> - </span>';
+  $elemento .= show_intervalo_data($data_inicial, $data_final);
+  $elemento .= '<span class="evento-local">' . $row_evento['local'] . '</span>';
+  $elemento .= show_action_icon('Editar', 'update.png', 'eventos_update.php', $row_evento['id'], $login);
+  $elemento .= show_action_icon('Apagar', 'delete.png', 'eventos_delete.php', $row_evento['id'], $login);
+  $elemento .= '</div>';
+  return $elemento;
+}
+
+function show_intervalo_data($data_inicial, $data_final) {
+  $elemento = '<span class="evento-data">';
+  if ($data_inicial == $data_final) {
+    $elemento .= utf8_encode(strftime('%d de %B de %Y', $data_inicial));
+  } else if ( strftime('%m', $data_inicial) == strftime('%m', $data_final) ) {
+    $elemento .= strftime('%d', $data_inicial) . '-' . strftime('%d ', $data_final);
+    $elemento .= utf8_encode(strftime('de %B de %Y', $data_final));
+  } else {
+    $elemento .= utf8_encode(strftime('%d de %B ', $data_inicial));
+    $elemento .= 'até ';
+    $elemento .= utf8_encode(strftime('%d de %B de %Y', $data_final));
+  }
+  $elemento .= ',</span>';
+  return $elemento;
+}
+
+function show_action_icon($action, $action_img, $action_file, $id, $login) {
+  $action_img_url = get_bloginfo('template_url') . '/imagens//' . $action_img;
+  $action_file_url = get_bloginfo('template_url') . '/' . $action_file;
+  $elemento = '<span class="action_icon eventos_icon">';
+  $elemento .= '<a class="icon ' . $login . '" href="' . $action_file_url . '?id=' . $id . '" title="' . $action . '">';
+  $elemento .= '<img src="' . $action_img_url . '" alt="' . $action . 'icon"/>';
+  $elemento .= '</a>';
+  $elemento .= '</span>';
+  return $elemento;
+}
