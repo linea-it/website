74b5dfe5287deed2b93d3c7cf20e1896ec722aa0
Leonardo Lacerda <leonardo.lacerda@linea.gov.br>
2018-07-09 11:55:50 -0300

    *Pagina de estatistica para ticket e para projetos. *PAgina de mosaico colocaborador. *Imagens para exemplo do mosaico.

---
+++ wp-content/themes/LIneA/estatistica_ticket.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Template Name: Estatistica de Ticket
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+?>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="/resources/demos/style.css">
+
+<?php get_header(); ?>
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<?php get_sidebar(); ?>
+<div class="breadcrumb"><a href="<?php bloginfo('url'); ?>">LIneA</a> » <a href="<?php bloginfo('url'); ?>/Ticket/" rel="category tag">Estatistica de Ticket</a></div>
+<div class="clearboth"></div> 
+  <div class="conteudo estatistica_ticket col-md-12"  role="main">
+     <h1>Relatório de Estatísticas Tickets - 2018</h1>  
+     <div class="content">
+  <div class="tabs-content">
+    <div class="tabs-menu clearfix">
+      <ul>
+        <li><a class="active-tab-menu" href="#" data-tab="tab1">Jan 2018</a></li>
+        <li><a href="#" data-tab="tab2">Fev 2018</a></li>
+        <li><a href="#" data-tab="tab3">Mar 2018</a></li>
+        <li><a href="#" data-tab="tab4">Abr 2018</a></li>
+        <li><a href="#" data-tab="tab5">Mai 2018</a></li>
+        <li><a href="#" data-tab="tab6">Jun 2018</a></li>
+        <li><a href="#" data-tab="tab7">Jul 2018</a></li>
+        <li><a href="#" data-tab="tab8">Ago 2018</a></li>
+        <li><a href="#" data-tab="tab9">Set 2018</a></li>
+        <li><a href="#" data-tab="tab10">Out 2018</a></li>
+        <li><a href="#" data-tab="tab11">Nov 2018</a></li>
+        <li><a href="#" data-tab="tab12">Dez 2018</a></li>
+      </ul>
+    </div> <!-- tabs-menu -->
+    
+    <div class="tab1 tabs first-tab">      
+   <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTQ3ksNTV-hwOEwFKW_rldP8-Jtlzh1IsU7yVQsNScXoI0M3u8pgLiFSx9sYphzw_ofno_e1fGiDkCN/embed?start=false&loop=true&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    </div> <!-- .tab1 -->
+    
+    <div class="tab2 tabs">            
+      <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQcpN3fq4sw0Geu6Z3Y5sSjZMGJnSQbMvnXFyzGJYnnh627p-F8e43kQAvmGgEhLbmbkpT1bdimtYy2/embed?start=false&loop=true&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    </div> <!-- .tab2 -->
+    
+    <div class="tab3 tabs">
+     <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vS9qMN3taRbKVEtSLN7C8d8NK517Jn-wH0TiEXcH6DZrKEWzg_hyu5FZ7PaQNqAb-qVZdvv4HHyGVRd/embed?start=false&loop=true&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    </div> <!-- .tab3 -->
+
+    <div class="tab4 tabs">
+     <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vR7V2MrMUUjGTXU195-anW_ld8--M-xDuEtKgK40SjYtS78iCpcbevkQrqzhe1kAYQW95SU2mGblwXo/embed?start=false&loop=true&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    </div> <!-- .tab4 -->
+
+    <div class="tab5 tabs">
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSYJA8o1rDGrXUyEnSTw4osD5UJStxRc8thterl3mwm3V5CPpzxwhZhtIxh5RwpZRppw9-2hKRNjpX2/embed?start=false&loop=true&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+
+    </div> <!-- .tab5 -->
+
+     <div class="tab6 tabs">
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRfTVZcm1rBlTwag_tz0xXAKJR1mRT2KMvVUQwva1jViZclT_y3ksuHCTstcP9LlLo3u3SuCLI_l-7d/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+
+    </div> <!-- .tab6 -->
+
+     <div class="tab7 tabs">
+   <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTvgqaiMSVn7xnePyWuOGXq_zzE1IueuIzp0TmSO_3DHtLlptsc7loXAEWktfbfx5CtdljMCq_JLZjm/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+
+    </div> <!-- .tab7 -->
+
+       <div class="tab8 tabs">
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRDZbGFPe9q2dZ_0hcKEFVHcZBeGGLFIZ_X8EokNYYDQCYc9Vsh031knzYZRCfUtm9O8viiQT1qpeAe/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    </div> <!-- .tab8 -->
+
+       <div class="tab9 tabs">
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ9VzodomaetuBw2ixOBNS6P7VQjCgt7FnLMRvCNW2yu5snOE3FatynbpuuV38LdnmR8UpGgf9v_Vhj/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    </div> <!-- .tab9 -->
+
+           <div class="tab10 tabs">
+  <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQM9WKp7jyETLtJLwUqixkxyBu_MuIzTEV15166g587ShVVw99UJUj2n2dP1KRqfatjTlC4WqGUAoOL/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+
+
+    </div> <!-- .tab10 -->
+
+              <div class="tab11 tabs">
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQc99xVcKlvRoE03lznzedhFQOTBhlGbrnQ0_yUiQuYB2A3bv17FPN7Z4fK9Vt2GwE0jCL4Rzklnmmi/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+
+
+    </div> <!-- .tab11 -->
+
+                  <div class="tab12 tabs">
+   <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQf5l8JuuYqzSy7nA_lrWArT3HaHpLxWsROsQ91jNT8s6LPCj9v-OZnyP3d2vi3L9oVOr-1Ja7Xb6Hy/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+
+
+    </div> <!-- .tab12 -->
+
+
+    
+  </div> <!-- .tabs -->
+</div> <!-- .content -->
+ 
+    </div>           
+<?php get_footer(); ?>
