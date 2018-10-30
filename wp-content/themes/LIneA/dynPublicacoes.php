ca2639b6f1e496887b29dd8b24a9153040d40138
webmaster <webmaster@webback4.linea.gov.br>
2018-10-30 14:15:41 -0300

    Separa publicacoes por ano

--- wp-content/themes/LIneA/dynPublicacoes.php
+++ wp-content/themes/LIneA/dynPublicacoes.php
@@ -22,6 +22,16 @@ Template Name: Publicacoes
 	    	$aceitos = array();
 	    	$publicados = array();
 
+            function count_by_year($lista, $ano){
+                $count = 0;
+                foreach($lista as $item){
+                    if ($item['ano'] == $ano) {
+                        $count++;
+                    }
+                }
+                return $count;
+            }
+
 	    	Database::disconnect();
 	    	if (is_user_logged_in()) {
 	       		$login = 'ativado';
@@ -31,7 +41,8 @@ Template Name: Publicacoes
 	    	echo '<h3>Publicações com participação de afiliados ao LIneA (2008 – Presente) <span class="countnum card">' . sprintf("%02d", count($result)) . '</h3>'; 
 	       	
 	       	echo (is_user_logged_in() ? '<a class="btn" href="'. get_bloginfo('template_url') .'/public_create.php"> Adicionar </a>' : '');
-
+            echo '<a class="btn" onclick="showAll(\'ano-container\')"> Mostrar </a>';
+            echo '<a class="btn" onclick="hideAll(\'ano-container\')"> Esconder </a>';
 
 	    	foreach ($result as $row) {
 	    		if ($row['status'] == 'submetido'){
@@ -46,7 +57,8 @@ Template Name: Publicacoes
 	    	// Submetidos
 
 	    	echo '<h3>Submetidos <span class="countnum card">' . sprintf("%02d", count($submetidos)) . '</span></h3>';
-	    	echo '<ol>';
+            
+            $ano = '';
 	    	foreach ($submetidos as $row) {
 	    		if ($row['link'] == ''){
 	    			$link = '';
@@ -54,16 +66,31 @@ Template Name: Publicacoes
 	    		} else {
 	    			$link = $row['link'];
 	    			$link_class = 'ativado';
-	    		}
+                }
+                if ($ano != $row['ano']){
+                    if ($ano != ''){
+                        echo '</div>';
+                        echo '<div class="clearboth"></div>';
+                    }
+                    $ano = $row['ano'];
+                    echo '<a onclick="toggleYear(\'submetidos-' . $ano . '\')">';
+                    echo '    <p class="ano-titulo">' . $ano . '</p>';
+                    echo '</a>';
+                    echo '<span class="countnum card">' . sprintf("%02d", count_by_year($submetidos, $ano)) . '</span>';
+                    echo '<div style="display: block" class="ano-container" id="submetidos-' . $ano . '">';
+                    echo '<ol>';
+                }
 	    		echo '<li><em>' . $row['titulo'] . ' ' . $row['autor'] . ' <strong>' . $row['ano'] . '</strong>, <strong>' . $row['revista'] .
 	    		'</strong>.</em> <a class=' . $link_class . ' href=' . $link . '> arXiv </a><a class="icon ' . $login . '" href="'. get_bloginfo('template_url') .'/public_update.php?id='. $row['id'] .'"><img src="' . get_bloginfo('template_url') . '/imagens/ic_create_white_24dp_2x.png" alt="Edit icon"/></a><a class="icon ' . $login . '" href="'. get_bloginfo('template_url') .'/public_delete.php?id='. $row['id'] .'"><img src="' . get_bloginfo('template_url') . '/imagens/ic_remove_circle_outline_white_24dp_2x.png" alt="Remove icon"/></a></li>';
-	    	}
-	    	echo '</ol>';	    	
+            }
+            echo '</ol>';
+            echo '</div>';
+	    		    	
 
 	    	// Aceitos
 
 	    	echo '<h3>Aceitos <span class="countnum card">' . sprintf("%02d", count($aceitos)) . '</span></h3>';
-	    	echo '<ol>';
+	    	$ano = '';
 	    	foreach ($aceitos as $row) {
 	    		if ($row['link'] == ''){
 	    			$link = '';
@@ -72,15 +99,29 @@ Template Name: Publicacoes
 	    			$link = $row['link'];
 	    			$link_class = 'ativado';
 	    		}
+	    		if ($ano != $row['ano']){
+                    if ($ano != ''){
+                        echo '</div>';
+                        echo '<div class="clearboth"></div>';
+                    }
+                    $ano = $row['ano'];
+                    echo '<a onclick="toggleYear(\'aceitos-' . $ano . '\')">';
+                    echo '    <p class="ano-titulo">' . $ano . '</p>';
+                    echo '</a>';
+                    echo '<span class="countnum card">' . sprintf("%02d", count_by_year($aceitos, $ano)) . '</span>';
+                    echo '<div style="display: block" class="ano-container" id="aceitos-' . $ano . '">';
+                    echo '<ol>';
+                }
 	    		echo '<li><em>' . $row['titulo'] . ' ' . $row['autor'] . ' <strong>' . $row['ano'] . '</strong>, <strong>' . $row['revista'] .
 	    		'</strong>.</em> <a class=' . $link_class . ' href=' . $link . '> arXiv </a><a class="icon ' . $login . '" href="'. get_bloginfo('template_url') .'/public_update.php?id='. $row['id'] .'"><img src="' . get_bloginfo('template_url') . '/imagens/ic_create_white_24dp_2x.png" alt="Edit icon"/></a><a class="icon ' . $login . '" href="'. get_bloginfo('template_url') .'/public_delete.php?id='. $row['id'] .'"><img src="' . get_bloginfo('template_url') . '/imagens/ic_remove_circle_outline_white_24dp_2x.png" alt="Remove icon"/></a></li>';
-	    	}
-	    	echo '</ol>';	    	
+            }
+            echo '</ol>';
+            echo '</div>';
 
 	    	// Publicados
 
 	    	echo '<h3>Publicados <span class="countnum card">' . sprintf("%02d", count($publicados)) . '</span></h3>';
-	    	echo '<ol>';
+	    	$ano = '';
 	    	foreach ($publicados as $row) {
 	    		if ($row['link'] == ''){
 	    			$link = '';
@@ -89,10 +130,24 @@ Template Name: Publicacoes
 	    			$link = $row['link'];
 	    			$link_class = 'ativado';
 	    		}
-	    		echo '<li><em>' . $row['titulo'] . ' ' . $row['autor'] . ' <strong>' . $row['ano'] . '</strong>, <strong><a class=' . $link_class . ' href=' . $link .
-	    		'>'. $row['revista'] . '</a></strong>, ' . $row['numpagina'] . '.</em><a class="icon ' . $login . '" href="'. get_bloginfo('template_url') .'/public_update.php?id='. $row['id'] .'"><img src="' . get_bloginfo('template_url') . '/imagens/ic_create_white_24dp_2x.png" alt="Edit icon"/></a><a class="icon ' . $login . '" href="'. get_bloginfo('template_url') .'/public_delete.php?id='. $row['id'] .'"><img src="' . get_bloginfo('template_url') . '/imagens/ic_remove_circle_outline_white_24dp_2x.png" alt="Remove icon"/></a></li>';
-	    	}
-	    	echo '</ol>';
+	    		if ($ano != $row['ano']){
+                    if ($ano != ''){
+                        echo '</div>';
+                        echo '<div class="clearboth"></div>';
+                    }
+                    $ano = $row['ano'];
+                    echo '<a onclick="toggleYear(\'publicados-' . $ano . '\')">';
+                    echo '    <p class="ano-titulo">' . $ano . '</p>';
+                    echo '</a>';
+                    echo '<span class="countnum card">' . sprintf("%02d", count_by_year($publicados, $ano)) . '</span>';
+                    echo '<div style="display: block" class="ano-container" id="publicados-' . $ano . '">';
+                    echo '<ol>';
+                }
+	    		echo '<li><em>' . $row['titulo'] . ' ' . $row['autor'] . ' <strong>' . $row['ano'] . '</strong>, <strong>' . $row['revista'] .
+	    		'</strong>.</em> <a class=' . $link_class . ' href=' . $link . '> arXiv </a><a class="icon ' . $login . '" href="'. get_bloginfo('template_url') .'/public_update.php?id='. $row['id'] .'"><img src="' . get_bloginfo('template_url') . '/imagens/ic_create_white_24dp_2x.png" alt="Edit icon"/></a><a class="icon ' . $login . '" href="'. get_bloginfo('template_url') .'/public_delete.php?id='. $row['id'] .'"><img src="' . get_bloginfo('template_url') . '/imagens/ic_remove_circle_outline_white_24dp_2x.png" alt="Remove icon"/></a></li>';
+            }
+            echo '</ol>';
+            echo '</div>';
 	           
         ?>
 
