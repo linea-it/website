b431891c25e3a167ea2559249b6c16a7705f6f20
Guilherme <guilherme.soares@linea.gov.br>
2019-01-28 15:52:39 +0000

    Adiciona card de servicos e parametros na funcao para gerar link nos cards

--- wp-content/themes/LIneA/home_functions.php
+++ wp-content/themes/LIneA/home_functions.php
@@ -1,19 +1,35 @@
 <?php
 
-function page_link_container($page_id) {
+function page_link_container($page_id, $title='', $link='', $image_id='') {
     $str = '';
     $page_data = get_post( $page_id );
     setup_postdata($page_data);
-    $thumbnail_tag = get_the_post_thumbnail($page_id, 'full');
+    if ($image_id == ''){
+        $thumbnail_tag = get_the_post_thumbnail($page_id, 'full');
+    } else {
+        $attach = wp_get_attachment_image_src($image_id, 'full');
+        if ($attach){
+            $thumbnail_tag = '<img src="' . $attach[0] . '">';
+        }
+    }
+
+    if ($title == ''){
+        $title = $page_data->post_title;
+    }
+
+    if ($link == ''){
+        $link = get_the_permalink($page_id);
+    }
+
     $str .= '<div class="page-link-container">';
-    $str .= '   <a class="page-card-link" href="' . get_the_permalink($page_id) . '">';
+    $str .= '   <a class="page-card-link" href="' . $link . '">';
     if ($thumbnail_tag) {
         $str .= '<div class="page-link-icon-container">';
         $str .= $thumbnail_tag;
         $str .= '</div>';
     }
     $str .= '   <h3 class="page-link-card-title">';
-    $str .= $page_data->post_title;
+    $str .= $title;
     $str .= '   </h3>';
     $str .= '   </a>';
     $str .= '</div>';
