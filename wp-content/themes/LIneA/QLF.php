f81c868a10681a021acc2b49eee96709952b3fb7
Leonardo Lacerda <leonardo.lacerda@linea.gov.br>
2019-02-28 15:58:14 -0300

    Paginas Landing-page colaboracao cientifica e Servico Publico

---
+++ wp-content/themes/LIneA/QLF.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: QLF
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+?>
+<?php get_header(); ?>
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
