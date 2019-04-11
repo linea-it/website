056f2e154d8628c7085911e1ca44434f4477d3bd
tumatihub <jgsoaresdias@gmail.com>
2019-04-11 13:51:30 -0300

    Revert "Landing page"

--- wp-content/themes/LIneA/general_service.php
+++ wp-content/themes/LIneA/general_service.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: General Service 
+ * Template Name: DESI - General Service  
  * @package WordPress
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
@@ -10,27 +10,5 @@
 <?php get_header(); ?>
 <?php header('Content-Type: text/html; charset=utf-8'); ?>
 <?php get_sidebar(); ?>
-<div class="home-card projects-card">
-            <?php
-            $mypages = get_pages( array( 'parent' => '7745', 'sort_column' => 'post_name', 'sort_order' => 'asc' ) );
-            foreach( $mypages as $page ) {
-                $title = $page->post_title; 
-                $content = $page->post_content;               
-                $link = get_the_permalink($page->ID);
-                $title = apply_filters( 'the_title', $title );
-                $thumbnail_tag = get_the_post_thumbnail($page->ID, 'full');
-                ?>
-                <div class="project-landing-Page">
-                    <a href="<?php echo $link; ?>">
-                         <h3 class="project-title-landing-Page"><?php echo $title ?></h3>
-                          <div class="project-logo-container-landing-Page">
-                            <?php echo $thumbnail_tag; ?>                          
-                        </div>  
-                        </a>
-                    <p class="project-landing-Page"><?php echo $content ?> </p>
-                </div>
-                <?php
-            }
-            ?>   
-</div> 
+
 <?php get_footer(); ?>
